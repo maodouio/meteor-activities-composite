573af659e06c3785f9e2d4a7af0d6a7a358dbf34
Douhengshan <13412133311@139.com>
2016-01-09 22:40:23 +0800

    change messages link of edit page

--- client/activities/activity_edit.js
+++ client/activities/activity_edit.js
@@ -26,9 +26,9 @@ AutoForm.hooks({
           console.log("e.userId = ", e.userId);
           list.push(e.userId);
         });
-        var content = "【活动已更新】“" + this.currentDoc.title + "”活动已更新，查看详情" + "http://x-lab.maodou.io/activities/" + this.docId;
+        var content = "【活动已更新】“" + this.currentDoc.title + "”活动已更新，查看详情" + "http://mbaas.maodou.io/activities/" + this.docId;
         console.log("debug====>",content);
-        console.log(list);
+        console.log('list is --------->', list);
         Meteor.call("multiSendMessage", list, content);
       }
 

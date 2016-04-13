b44b0705f8ec27eac60664ac9c6b967ed2620d98
FunkySamuel37 <masterzhiwei@gmail.com>
2016-04-13 15:50:25 +0800

    set permissions

--- server/permissions/activities.js
+++ server/permissions/activities.js
@@ -1,16 +1,19 @@
 Activities.allow({
   insert: function (userId, doc) {
     // Free-for-all!
-    return true;
+    if (userId) {
+      return true;
+    }
+    return false;
   },
   update: function (userId, doc, fields, modifier) {
-    if (userId == doc.userId) {
+    if (userId == doc.userId || Roles.userIsInRole(user.user._id, ['admin'])) {
       return true;
     }
     return false;
   },
   remove: function (userId, doc) {
-    if (userId == doc.userId) {
+    if (userId == doc.userId || Roles.userIsInRole(user.user._id, ['admin'])) {
       return true;
     }
     return false;
